f5f2711c5083eb725112b3563d39d2c4f4b4dd16
samarth10 <samarth@umail.ucsb.edu>
2017-12-05 20:57:09 -0800

    Update Trader.java

--- src/Trader.java
+++ src/Trader.java
@@ -1,3 +1,37 @@
 public class Trader {
+
+	public static void login(String username, String password) {
+		String sql = "";
+		JDBC.connection.statement.executeQuery(sql);
+	}
 	
+	public static void showBalance() {
+		String sql = "";
+		JDBC.connection.statement.executeQuery(sql);
+	}
+
+	public static void showTransactionHistory() {
+		String sql = "";
+		JDBC.connection.statement.executeQuery(sql);
+	}
+
+	public static showStockInfo(String symbol) {
+		String sql = "";
+		JDBC.connection.statement.executeQuery(sql);
+	}
+
+	public static listMovieInfo() {
+		String sql = "";
+		JDBC.connection.statement.executeQuery(sql);
+	}
+
+	public static showTopMovies() {
+		String sql = "";
+		JDBC.connection.statement.executeQuery(sql);
+	}
+
+	public static showReviews(String movieName) {
+		String sql = "";
+		JDBC.connection.statement.executeQuery(sql);
+	}
 }
