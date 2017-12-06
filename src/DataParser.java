ecb49722484af642b342acfeb75e566f72d69cc9
bpatient24@gmail.com <bpatient@umail.ucsb.edu>
2017-12-05 16:39:38 -0800

    added data parser

---
+++ src/DataParser.java
@@ -0,0 +1,85 @@
+import java.nio.charset.CharacterCodingException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Path;
+
+public class DataParser{
+	int numArgs;
+	String sql;
+	String tableName;
+	String[] parameters;
+	ArrayList<String> queries = new ArrayList<String>();
+
+	public DataParser(){
+		numArgs=-1;
+		sql="";
+	}
+
+	public DataParser(String dataset){
+		parseFile(dataset);
+	}
+
+	public String getTable(String filename){
+		return filename.substring(0, filename.indexOf(".")); 
+	}
+
+	public void formSQL(String[] data){
+		sql = "insert into ";
+		sql += tableName;
+		sql += "(";
+		for(int i=0; i < numArgs; i++){
+			if(i != numArgs-1){
+				sql += parameters[i];
+				sql += ",";
+			} 
+			else if(i == numArgs-1){
+				sql += parameters[i];
+				sql += ") ";
+			}
+		}
+		sql += "values (";
+		for(int i=0; i < numArgs; i++){
+			if(i != numArgs-1){
+				sql += data[i];
+				sql += ",";
+			} 
+			else if(i == numArgs-1){
+				sql += data[i];
+				sql += ")";
+			}
+		}
+		sql += ";";
+		queries.add(sql);
+
+	}
+
+	public void parseFile(String filename){
+		tableName = getTable(filename);
+		try (BufferedReader br = new BufferedReader(new FileReader(filename))){
+
+            	String currentLine;
+            	boolean firstLine = true;
+            	while ((currentLine = br.readLine()) != null) {
+            		if(firstLine){
+            			parameters = currentLine.split(",");
+            			numArgs = parameters.length;
+            			firstLine = false;
+            		}
+            		else{
+            			String[] temp = currentLine.split(",");
+            			formSQL(temp);
+            		}
+            	}
+
+        	} catch (IOException e) {
+          		e.printStackTrace();
+        	} 
+	}
+}
--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,21 @@ public class Main {
 	static UI ui; 
 	static JDBC db;
     public static void main(String[] args) {
-    	db = new JDBC();
-    	ui = new UI();
-    	ui.begin();
+    	if(args.length == 1){
+    		DataParser parser;
+    		parser = new DataParser(args[0]);
+    		//System.out.println(parser.numArgs);
+    		for(int i=0; i < parser.queries.size(); i++){
+    			String tmp = parser.queries.get(i);
+    			System.out.println(tmp);
+    		}
+    	}
+    	else{
+	    	db = new JDBC();
+	    	ui = new UI();
+	    	ui.begin();
 
-    	db.endSession();
+	    	db.endSession();
+    	}
     }
 }
